cecffd3db60f379b3d9443e6cf60b2ea5cde1bd7
Fash Mayor <mayowasamson03@gmail.com>
2025-09-09 05:17:24 +0100

    created ProductCard.jsx and products.js
    Created product.js and added a few demo products.
    Created ProductCard.jsx to recieve objects from products.js and display on the site.

---
+++ src/data/products.js
@@ -0,0 +1,43 @@
+export const products = [
+  {
+    id: 1,
+    name: "Chocolate Birthday Cake",
+    price: 45.99,
+    category: "Cakes",
+    image: "/assets/chocolate_bithday_cake.webp",
+    description: "Rich chocolate cake",
+    ingredients: ["flour", "cocoa", "eggs"],
+    allergens: ["gluten", "dairy", "eggs"]
+  },
+  {
+    id: 2,
+    name: "Blueberry Muffin",
+    price: 3.99,
+    category: "Pastries",
+    image: "/assets/blueberry-muffin.jpg",
+    description: "Fresh blueberry muffins baked daily.",
+    ingredients: ["flour", "blueberries", "sugar", "eggs"],
+    allergens: ["gluten", "eggs"]
+  },
+  {
+    id: 3,
+    name: "Chocolate Muffin",
+    price: 5.99,
+    category: "Pastries",
+    image: "/assets/cut-vanilla-cake.webp",
+    description: "Fresh blueberry muffins baked daily.",
+    ingredients: ["flour", "blueberries", "sugar", "eggs"],
+    allergens: ["gluten", "eggs"]
+  },
+  {
+    id: 4,
+    name: "Blueberry cake",
+    price: 5.99,
+    category: "Pastries",
+    image: "/assets/moana_round_cake.webp",
+    description: "Fresh blueberry muffins baked daily.",
+    ingredients: ["flour", "blueberries", "sugar", "eggs"],
+    allergens: ["gluten", "eggs"]
+  }
+  // Add more products...
+];
